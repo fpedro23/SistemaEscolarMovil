84cf230d63bd40e3370529f96ceca3c0b99e4553
fpedro23 <fpedro23@gmail.com>
2015-02-12 17:45:07 -0600

    Initial Commit

---
+++ SistemaEscolarMovil/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SistemaEscolarMovil
+//
+//  Created by Pedro Contreras Nava on 12/02/15.
+//  Copyright (c) 2015 Pedro Contreras Nava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
